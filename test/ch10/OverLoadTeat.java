ee1b86bc52f0a8a6f128313705637957ed22c0c4
hdw123456 <1658642701@qq.com>
2021-01-28 16:29:34 +0800

    1

--- test/ch10/OverLoadTeat.java
+++ test/ch10/OverLoadTeat.java
@@ -1,5 +1,4 @@
 package test.ch10;
-
 public class OverLoadTeat {
     public static int add(int a, int b) {
         return a + b;
@@ -17,10 +16,18 @@ public class OverLoadTeat {
         return a + b;
     }
 
+    public static int add(int... a) {
+        int s = 0;
+        for (int x : a)
+            s += x;
+        return s;
+    }
+
     public static void main(String[] args) {
         System.out.println(add(1, 2));
         System.out.println(add(2.1, 3.5));
         System.out.println(add(3.4, 2));
         System.out.println(add(5, 2.7));
+        System.out.println(add(10, 20, 30, 40, 80));
     }
 }
