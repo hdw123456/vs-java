b1db481bad372022ae266798ec8db9522121c7bb
hdw123456 <1658642701@qq.com>
2021-01-28 16:12:06 +0800

    1

---
+++ test/ch10/OverLoadTeat.java
@@ -0,0 +1,26 @@
+package test.ch10;
+
+public class OverLoadTeat {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static double add(double a, int b) {
+        return a + b;
+    }
+
+    public static double add(int a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(1, 2));
+        System.out.println(add(2.1, 3.5));
+        System.out.println(add(3.4, 2));
+        System.out.println(add(5, 2.7));
+    }
+}
